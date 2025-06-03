b4864dc886af93bfa898625d322ff6d1792c102e
Frahyang <frahyang2005@gmail.com>
2025-06-03 12:35:27 +0700

    CICD
    Signed-off-by: Frahyang <frahyang2005@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://e2425-wads-l4bcg3-client.csbihub.id',
   credentials: true
 }));
 app.use(express.json());
